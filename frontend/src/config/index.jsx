b1a424367ddda6c4f77880923f5471279186460d
Ayush nema <nemaayush22@gmail.com>
2026-02-10 15:21:57 +0530

    read for depploy

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios"
 
-export const base_URL = 'http://localhost:9000';
+export const base_URL = 'https://socialmedia-84fy.onrender.com';
 
  export const clientServer = axios.create({
     baseURL : base_URL,
